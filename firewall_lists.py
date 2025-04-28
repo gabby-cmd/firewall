49b7332d180458ebb26ee35bf42fb4826fa91a88
Gabrielle Prabhu <gabbyprabhu@gmail.com>
2025-04-28 14:52:26 -0400

    Create firewall_lists.py

---
+++ firewall_lists.py
@@ -0,0 +1,25 @@
+
+# 🚫 Expanded Block List (bad/toxic words)
+BLOCK_LIST = [
+    "hack", "bomb", "attack", "self harm", "weapon", "suicide", "kill", "murder",
+    "assault", "terror", "explosive", "drug", "cocaine", "weed", "abuse", "porn",
+    "violence", "hate", "racist", "harass", "guns", "knife", "hostage", "theft",
+    "fraud", "scam", "molest", "rape", "abduction", "blackmail", "extortion",
+    "stabbing", "shooting", "slaughter", "robbery", "arson", "vandalism", "trafficking",
+    "exploit", "radical", "threat", "battery", "manslaughter", "lynch", "hostility",
+    "riot", "insult", "war", "bloodshed"
+]
+
+# ✅ Expanded Allow List (finance-related words)
+ALLOW_LIST = [
+    "investment", "stocks", "bonds", "mutual funds", "portfolio", "dividends", 
+    "NASDAQ", "S&P 500", "Dow Jones", "commodities", "forex", "cryptocurrency",
+    "bitcoin", "Ethereum", "bull market", "bear market", "options trading",
+    "futures", "ETFs", "REITs", "asset allocation", "risk management",
+    "hedging", "liquidity", "market cap", "valuation", "earnings report",
+    "IPO", "SEC", "federal reserve", "inflation", "interest rates",
+    "monetary policy", "fiscal policy", "GDP", "unemployment rate", "treasury bonds",
+    "yield curve", "short selling", "buyback", "dividend yield", "debt market",
+    "private equity", "venture capital", "angel investing", "retirement fund",
+    "401k", "IRA", "capital gains", "tax planning"
+]
